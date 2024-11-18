da16bf817bc68deaaca04daa1b9ef2f7056f0626
st183570 <st183570@stud.uni-stuttgart.de>
2024-11-18 23:58:41 +0100

    -

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname));
+
+
+app.get("/api", (req, res) => {
+    res.json({ "users" : ["John", "Doe", "Jane", "tet"] , "title": "Import Template", "items": ["Item 1", "Item 2", "Item 3"], "onNew": "onNew", "onImport": "onImport" });
+
+});
+
+
+// Fügen Sie diese Zeile hinzu, um sicherzustellen, dass alle anderen Routen auf Ihre index.html Datei verweisen
+//app.get('*', (req, res) => {
+//    res.sendFile(path.join(__dirname, 'index.html'));
+//});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
